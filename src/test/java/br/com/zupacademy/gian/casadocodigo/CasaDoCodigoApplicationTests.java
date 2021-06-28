a41ebd3e3e442604371381f9c95729ba4bc7d0da
gian.souza <gianmarcelsouza@gmail.com>
2021-06-28 09:17:26 -0300

    setup do projeto

---
+++ src/test/java/br/com/zupacademy/gian/casadocodigo/CasaDoCodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.gian.casadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaDoCodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
